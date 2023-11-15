0ff7511c66de83992d8db7cddd1dfc63abe599ee
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2023-11-15 18:25:27 +0530

    Removed Page context

--- samples/.NET/NUnit/Tests.cs
+++ samples/.NET/NUnit/Tests.cs
@@ -14,24 +14,14 @@ public class Tests : PageTest
     public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
     {
         
-        var context = await Browser.NewContextAsync();
 
-        // //Start tracing.
-        // await context.Tracing.StartAsync(new()
-        // {
-        //     Screenshots = true,
-        //     Snapshots = true,
-        //     Sources = true
-        // });
-
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://playwright.dev");
+        await Page.GotoAsync("https://playwright.dev");
 
         // Expect a title "to contain" a substring.
-        await Expect(page).ToHaveTitleAsync(new Regex("Playwright"));
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
 
         // create a locator
-        var getStarted = page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
 
         // Expect an attribute "to be strictly equal" to the value.
         await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
@@ -40,13 +30,7 @@ public class Tests : PageTest
         await getStarted.ClickAsync();
 
         // Expects the URL to contain intro.
-        await Expect(page).ToHaveURLAsync(new Regex(".*intro"));
-
-    //    // Stop tracing and export it into a zip archive.
-    //     await context.Tracing.StopAsync(new()
-    //     {
-    //         Path = "<Add absolute path>" + "trace.zip"
-    //     });
+        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
 
     }
 }
