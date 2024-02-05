3996177f07470f2131bbb2f78cc121f031ba47d5
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2024-02-05 14:48:33 +0530

    Update playwright.config.ts

--- samples/get-started/playwright.config.ts
+++ samples/get-started/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html'],['blob'],['github']],
   use: {
     trace: 'on-first-retry',
   },
