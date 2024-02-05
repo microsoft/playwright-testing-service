f721ea772056e748a63d46c59e639ad5c03e4bb0
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2024-02-05 14:50:36 +0530

    Update example.spec.ts

--- samples/get-started/tests/example.spec.ts
+++ samples/get-started/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 // This sample simulates a larger test suite
-const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "100");
+const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "1");
 for (var i = 0; i < TEST_ITERATIONS; i++) {
 
   test('has title ' + i, async ({ page }) => {
@@ -19,6 +19,9 @@ for (var i = 0; i < TEST_ITERATIONS; i++) {
 
     // Expects the URL to contain intro.
     await expect(page).toHaveURL(/.*intro/);
+    
+    const randomResult = Math.random();
+    expect(randomResult).toBeGreaterThan(0.5);
   });
 
 }
