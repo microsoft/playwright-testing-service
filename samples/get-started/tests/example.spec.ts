8e342645121b71a6ae633e052927eb20f5b86b60
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2024-03-13 17:08:55 +0530

    Update example.spec.ts

--- samples/get-started/tests/example.spec.ts
+++ samples/get-started/tests/example.spec.ts
@@ -20,8 +20,8 @@ for (var i = 0; i < TEST_ITERATIONS; i++) {
     // Expects the URL to contain intro.
     await expect(page).toHaveURL(/.*intro/);
     
-    // const randomResult = Math.random();
-    // expect(randomResult).toBeGreaterThan(0.5);
+    const randomResult = Math.random();
+    expect(randomResult).toBeGreaterThan(0.4);
   });
 
 }
