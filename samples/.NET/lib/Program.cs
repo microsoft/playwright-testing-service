246d3edfa5abe39713549676126b57c3fa2f7a50
Puneet Agarwal <28705989+puagarwa@users.noreply.github.com>
2025-07-24 23:34:34 +0530

    Adding dontet library sample (#200)

---
+++ samples/.NET/lib/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using System.Runtime.InteropServices;
+
+// Function to get connection options
+static (string wsEndpoint, BrowserTypeConnectOptions options) GetConnectOptions()
+{
+    var runId = Guid.NewGuid().ToString();
+    var os = "linux"; // Change to "windows" if wants to run on Windows
+    var apiVersion = "2023-10-01-preview";// DONOT change
+    
+    var serviceUrl = Environment.GetEnvironmentVariable("PLAYWRIGHT_SERVICE_URL") ?? "";
+    var accessToken = Environment.GetEnvironmentVariable("PLAYWRIGHT_SERVICE_ACCESS_TOKEN") ?? "";
+    
+    var wsEndpoint = $"{serviceUrl}?runId={Uri.EscapeDataString(runId)}&os={os}&api-version={apiVersion}";
+    
+    var options = new BrowserTypeConnectOptions
+    {
+        Timeout = 3 * 60 * 1000, // 3 minutes
+        Headers = new Dictionary<string, string>
+        {
+            { "Authorization", $"Bearer {accessToken}" }
+        },
+        ExposeNetwork = "<loopback>", // Use loopback to expose network
+    };
+    
+    return (wsEndpoint, options);
+}
+
+using var playwright = await Playwright.CreateAsync();
+
+// Get connection options
+var (wsEndpoint, connectOptions) = GetConnectOptions();
+
+await using var browser = await playwright.Chromium.ConnectAsync(wsEndpoint, connectOptions);
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+var title = await page.TitleAsync();
+Console.WriteLine($"Title: {title}");
+await page.ScreenshotAsync(new()
+{
+    Path = "screenshot.png"
+});
