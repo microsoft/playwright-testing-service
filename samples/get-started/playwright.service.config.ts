4624fab076e13c4b4df9bf9cc7fa774b50f964b8
Puneet Agarwal <puagarwa@microsoft.com>
2025-08-29 16:38:15 +0530

    update stable package

--- samples/get-started/playwright.service.config.ts
+++ samples/get-started/playwright.service.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from '@playwright/test';
-import { getServiceConfig, ServiceOS } from '@azure/playwright';
+import { createAzurePlaywrightConfig, ServiceOS } from '@azure/playwright';
 import config from './playwright.config';
 import { DefaultAzureCredential } from '@azure/identity';
 
 /* Learn more about service configuration at https://aka.ms/mpt/config */
 export default defineConfig(
   config,
-  getServiceConfig(config, {
+  createAzurePlaywrightConfig(config, {
     exposeNetwork: '<loopback>',
-    timeout: 30000,
+    connectTimeout: 30000,
     os: ServiceOS.LINUX,
-    useCloudHostedBrowsers: true,
     credential: new DefaultAzureCredential() // if serviceAuthType is ENTRA_ID (default)
   })
 );
